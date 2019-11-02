0b43a5c9080070e091b1c031a54904558354388e
p.grigoriev <p.grigoriev@bizon.help>
2019-11-02 16:49:12 +0300

    style(melody): remove extra brackets

--- src/components/welcome-screen/welcome-screen.tsx
+++ src/components/welcome-screen/welcome-screen.tsx
@@ -7,7 +7,7 @@ export const WelcomeScreen = (props: {gameTime: number; maxErrors: number}): any
   return (
     <section className="welcome">
       <div className="welcome__logo"><img src="img/melody-logo.png" alt="Угадай мелодию" width={ 186 } height={ 83 } /></div>
-      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <button type="button" className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
       <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
@@ -18,9 +18,7 @@ export const WelcomeScreen = (props: {gameTime: number; maxErrors: number}): any
         </li>
         <li>
 Можно допустить
-          { ' ' }
           { maxErrors }
-          { ' ' }
 ошибки.
         </li>
       </ul>
